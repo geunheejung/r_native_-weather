6791a16dbfeb05753de65010df5ee56a587317e1
geunheejung <geuni1013@gmail.com>
2018-05-23 18:45:34 +0900

    3-1:: 앱 하단에 Loading Text 추가 및 스타일 설정

--- App.js
+++ App.js
@@ -1,37 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+    };
+  }
+
   render() {
+    const { isLoaded } = this.state;
     return (
       // View 컴포넌트를 이옹하면 ios/android 환경에 따라 objective-c or java 로 변
       <View style={styles.container}>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
-        <View style={styles.redView}>Red</View>
-        <View style={styles.yellowView}>Yellow</View>
+        {isLoaded
+          ? null
+          : <View style={styles.loading}>
+              <Text style={styles.loadingText}>Getting the weather</Text>
+          </View>
+        }
       </View>
     );
   }
@@ -41,20 +29,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'space-around',
-    alignItems: 'flex-end',
-    flexDirection: 'row',
-    flexWrap: 'nowrap'
   },
-  redView: {
-    height: 50,
-    width: 50,
-    backgroundColor: 'red',
+  loading: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingLeft: 25,
+    // padding: 40 30 50 60 불가능
+    backgroundColor: '#FDF6AA'
   },
-  yellowView: {
-    height: 50,
-    width: 50,
-    backgroundColor: 'yellow',
+  loadingText: {
+    marginBottom: 100,
+    fontSize: 38,
   }
 });
 
